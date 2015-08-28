73aff8347232db02d5e7e494c27a50ba0dd56a93
DearMe <ruanqixiang@gmail.com>
2015-08-28 09:55:36 +0800

    add BST

---
+++ src/sort/ST.java
@@ -0,0 +1,15 @@
+package sort;
+
+/**
+ * Created by ruanqx on 2015/8/19.
+ */
+public class ST<Key, Value>{
+    ST(){}
+    void put(Key key, Value value){}
+    Value get(Key key){return null;}
+    void delete(Key key){}
+    boolean contains(Key key){return false;}
+    boolean isEmpty(){return true;}
+    int size(){return 0;}
+    Iterable<Key> keys(){return null;}
+}
